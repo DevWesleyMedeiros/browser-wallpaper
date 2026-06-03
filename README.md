2e8657561be4fbdd7f358cc9c8f5f56ed407f743
Weslei Medeiros da Rosa <139824516+DevWesleyMedeiros@users.noreply.github.com>
2026-06-02 23:03:35 -0300

    Melhorias: performance, design e novas funcionalidades

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# projeto-pratico-com-classes
- Tela de descanso
+# Tela de Descanso - Bouncing Balls
+
+Um projeto prático e visualmente agradável de bolas coloridas que quicam na tela, perfeito para usar como papel de parede ou tela de descanso!
+
+## Stack Utilizada
+- **HTML5** - Estrutura da página
+- **CSS3** - Estilização e animações
+- **JavaScript (ES6+)** - Lógica de programação orientada a objetos
+
+## Funcionalidades
+- Adicionar múltiplas bolas coloridas de uma vez
+- Controlar a velocidade das bolas
+- Definir tamanho mínimo e máximo das bolas
+- Opção de mostrar trilhas das bolas
+- Painel de controles ocultável (perfeito para usar como wallpaper)
+- Animações suaves e design moderno
+
+## O que pode ser estudado neste projeto?
+1. **Programação Orientada a Objetos (POO)** - Implementação da classe `Bola`
+2. **Manipulação do DOM** - Criação e remoção de elementos dinamicamente
+3. **requestAnimationFrame** - Animações performáticas no navegador
+4. **CSS Animations e Transitions** - Efeitos visuais suaves
+5. **Event Listeners** - Interação com o usuário
+6. **Resposividade** - Adaptação a diferentes tamanhos de tela
+7. **Gerenciamento de estado** - Controle de array de objetos
+
+## Como clonar e usar?
+
+1. **Clone o repositório**
+   ```bash
+   git clone https://github.com/wesleymartins15/projeto-pratico-com-classes.git
+   cd projeto-pratico-com-classes
+   ```
+
+2. **Abra o arquivo**
+   Basta abrir o arquivo `index.html` em qualquer navegador moderno!
+
+## Como usar como wallpaper?
+1. Abra o arquivo `index.html` no navegador
+2. Clique no botão ⚙️ para ocultar os controles
+3. Coloque o navegador em tela cheia (F11)
+4. Adicione as bolas e aproveite!
--- script/script.js
+++ script/script.js
@@ -3,90 +3,169 @@ const numObjetos = document.getElementById('num_objetos')
 const txtQtde = document.getElementById('txt_qtde')
 const btnAdd = document.getElementById('btn_add')
 const btnRemover = document.getElementById('btn_remover')
-
-
-class Bola{
-    constructor(arrayBolas, palco){
-        this.tamanho = Math.floor(Math.random()*15)+10
-        this.r = Math.floor(Math.random()*255)
-        this.g = Math.floor(Math.random()*255)
-        this.b = Math.floor(Math.random()*255)
-        this.posicaoX = Math.floor(Math.random()*(larguraPalco - this.tamanho))
-        this.posicaoY = Math.floor(Math.random()*(alturaPalco - this.tamanho))
-        this.velocidadeX = Math.floor(Math.random()*2)+0.5
-        this.velocidadeY = Math.floor(Math.random()*2)+0.5
-        this.direcaoX = Math.floor(Math.random()*10) > 5? 1 : -1
-        this.direcaoY = Math.floor(Math.random()*10) > 5? 1 : -1
-        this.palco = palco
-        this.arrayBolas = arrayBolas
-        this.id = Date.now() +"_"+Math.floor(Math.random()*100000000000)
-        this.desenhar()
-        this.controle = setInterval(this.controlar, 10)
-        this.eu = document.getElementById(this.id)
-    }
-    minhaPosicao = ()=>{
-        return this.arrayBolas.indexOf(this)
-    }
-    remover = ()=>{
-        clearInterval(this.controle)
-        bolas = bolas.filter((bola)=>{
-            if(bola.id != this.id){
-                return bola
-            }
-        })
-        this.eu.remove()
-        numBola--
-        numObjetos.innerHTML = numBola
-    }
-    desenhar = ()=>{
-        const div = document.createElement("div")
-        div.setAttribute("id", this.id)
-        div.setAttribute("class", "bola")
-        div.setAttribute("style", `left:${this.posicaoX}px; top:${this.posicaoY}px;width:${this.tamanho}px;height:${this.tamanho}px;background-color:rgb(${this.r}, ${this.g}, ${this.b});`)
-        this.palco.appendChild(div)
-    }
-    controleBordas = ()=>{
-        if(this.posicaoX + this.tamanho >= larguraPalco){
-            this.direcaoX = -1
-        }else if(this.posicaoX <= 0){
-            this.direcaoX = 1
-        }
-        if(this.posicaoY + this.tamanho >= alturaPalco){
-            this.direcaoY = -1
-        }else if(this.posicaoY <= 0){
-            this.direcaoY = 1
-        }
-    }
-    controlar = ()=>{
-        this.controleBordas()
-        this.posicaoX += this.direcaoX * this.velocidadeX
-        this.posicaoY += this.direcaoY * this.velocidadeY
-        this.eu.setAttribute("style", `left:${this.posicaoX}px; top:${this.posicaoY}px;width:${this.tamanho}px;height:${this.tamanho}px;background-color:rgb(${this.r}, ${this.g}, ${this.b});`)
-        if((this.posicaoX > larguraPalco)||(this.posicaoY > alturaPalco)){
-            this.remover()
-        }
-    }
-}
+const btnToggleControles = document.getElementById('btn_toggle_controles')
+const controles = document.getElementById('controles')
+const txtVelocidade = document.getElementById('txt_velocidade')
+const ckbTrails = document.getElementById('ckb_trails')
+const txtTamanhoMin = document.getElementById('txt_tamanho_min')
+const txtTamanhoMax = document.getElementById('txt_tamanho_max')
 
 let larguraPalco = palco.offsetWidth
 let alturaPalco = palco.offsetHeight
 
 let bolas = []
-let numBola = 0 
+let numBola = 0
+let animationId = null
+let showTrails = false
 
-window.addEventListener("resize", ()=>{
-    larguraPalco = palco.offsetWidth
-    alturaPalco = palco.offsetHeight
-})
-btnAdd.addEventListener("click", ()=>{
-    let qtde = txtQtde.value
-    for(i = 0; i < qtde; i++){
-        bolas.push(new Bola(bolas, palco))
+class Bola {
+  constructor(arrayBolas, palco) {
+    this.tamanhoMin = parseInt(txtTamanhoMin.value) || 10
+    this.tamanhoMax = parseInt(txtTamanhoMax.value) || 25
+    this.tamanho =
+      Math.floor(Math.random() * (this.tamanhoMax - this.tamanhoMin + 1)) + this.tamanhoMin
+    this.r = Math.floor(Math.random() * 255)
+    this.g = Math.floor(Math.random() * 255)
+    this.b = Math.floor(Math.random() * 255)
+    this.posicaoX = Math.floor(Math.random() * (larguraPalco - this.tamanho))
+    this.posicaoY = Math.floor(Math.random() * (alturaPalco - this.tamanho))
+    this.velocidadeBase = parseFloat(txtVelocidade.value) || 1
+    this.velocidadeX = Math.random() * this.velocidadeBase + 0.5
+    this.velocidadeY = Math.random() * this.velocidadeBase + 0.5
+    this.direcaoX = Math.random() > 0.5 ? 1 : -1
+    this.direcaoY = Math.random() > 0.5 ? 1 : -1
+    this.palco = palco
+    this.arrayBolas = arrayBolas
+    this.id = Date.now() + '_' + Math.floor(Math.random() * 100000000000)
+    this.trail = []
+    this.maxTrailLength = 10
+    this.desenhar()
+  }
+
+  minhaPosicao() {
+    return this.arrayBolas.indexOf(this)
+  }
+
+  remover() {
+    if (this.eu) {
+      this.eu.remove()
     }
+    if (this.trailElements) {
+      this.trailElements.forEach((el) => el.remove())
+    }
+  }
+
+  desenhar() {
+    const div = document.createElement('div')
+    div.setAttribute('id', this.id)
+    div.setAttribute('class', 'bola')
+    div.setAttribute(
+      'style',
+      `left:${this.posicaoX}px; top:${this.posicaoY}px;width:${this.tamanho}px;height:${this.tamanho}px;background-color:rgb(${this.r}, ${this.g}, ${this.b});box-shadow: 0 0 ${this.tamanho / 2}px rgb(${this.r}, ${this.g}, ${this.b});`,
+    )
+    this.palco.appendChild(div)
+    this.eu = document.getElementById(this.id)
+
+    this.trailElements = []
+    for (let i = 0; i < this.maxTrailLength; i++) {
+      const trailDiv = document.createElement('div')
+      trailDiv.setAttribute('class', 'bola-trail')
+      trailDiv.style.width = `${this.tamanho}px`
+      trailDiv.style.height = `${this.tamanho}px`
+      trailDiv.style.backgroundColor = `rgb(${this.r}, ${this.g}, ${this.b})`
+      trailDiv.style.opacity = 0
+      this.palco.appendChild(trailDiv)
+      this.trailElements.push(trailDiv)
+    }
+  }
+
+  controleBordas() {
+    if (this.posicaoX + this.tamanho >= larguraPalco) {
+      this.direcaoX = -1
+    } else if (this.posicaoX <= 0) {
+      this.direcaoX = 1
+    }
+    if (this.posicaoY + this.tamanho >= alturaPalco) {
+      this.direcaoY = -1
+    } else if (this.posicaoY <= 0) {
+      this.direcaoY = 1
+    }
+  }
+
+  atualizar() {
+    this.controleBordas()
+
+    this.trail.unshift({ x: this.posicaoX, y: this.posicaoY })
+    if (this.trail.length > this.maxTrailLength) {
+      this.trail.pop()
+    }
+
+    this.posicaoX += this.direcaoX * this.velocidadeX
+    this.posicaoY += this.direcaoY * this.velocidadeY
+
+    this.eu.style.left = `${this.posicaoX}px`
+    this.eu.style.top = `${this.posicaoY}px`
+
+    if (showTrails) {
+      this.trail.forEach((pos, index) => {
+        if (this.trailElements[index]) {
+          this.trailElements[index].style.left = `${pos.x}px`
+          this.trailElements[index].style.top = `${pos.y}px`
+          this.trailElements[index].style.opacity =
+            ((this.maxTrailLength - index) / this.maxTrailLength) * 0.5
+          this.trailElements[index].style.width =
+            `${this.tamanho * (1 - index / this.maxTrailLength)}px`
+          this.trailElements[index].style.height =
+            `${this.tamanho * (1 - index / this.maxTrailLength)}px`
+        }
+      })
+    } else {
+      this.trailElements.forEach((el) => (el.style.opacity = 0))
+    }
+  }
+}
+
+function animate() {
+  bolas.forEach((bola) => bola.atualizar())
+  animationId = requestAnimationFrame(animate)
+}
+
+function iniciarAnimacao() {
+  if (!animationId) {
+    animate()
+  }
+}
+
+window.addEventListener('resize', () => {
+  larguraPalco = palco.offsetWidth
+  alturaPalco = palco.offsetHeight
 })
 
-btnRemover.addEventListener("click", ()=>{
-    bolas.map((bola)=>{
-        bola.remover()
-    })
+btnAdd.addEventListener('click', () => {
+  let qtde = parseInt(txtQtde.value) || 0
+  for (let i = 0; i < qtde; i++) {
+    bolas.push(new Bola(bolas, palco))
+    numBola++
+  }
+  numObjetos.innerHTML = numBola
+  iniciarAnimacao()
+})
+
+btnRemover.addEventListener('click', () => {
+  bolas.forEach((bola) => bola.remover())
+  bolas = []
+  numBola = 0
+  numObjetos.innerHTML = numBola
+  if (animationId) {
+    cancelAnimationFrame(animationId)
+    animationId = null
+  }
+})
+
+btnToggleControles.addEventListener('click', () => {
+  controles.classList.toggle('hidden')
+})
+
+ckbTrails.addEventListener('change', (e) => {
+  showTrails = e.target.checked
 })
